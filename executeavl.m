5f05375e2a41c9307ec56249ebfe401252c88d8e
pekpuglia <pepuglia@gmail.com>
2022-10-17 09:31:45 -0300

    fixed file cleanup

--- executeavl.m
+++ executeavl.m
@@ -1,14 +1,16 @@
 function executeavl(plane, cases)
+    clean_files();
     %por ora, assume que todos os casos são level flight
     plane_avl_file(plane);
     run_fn = run_avl_file(plane, cases);
     %avl espera que não existam arquivos com os mesmos nomes dos arquivos
     %de saída
-    clean_output_files();
     avlcall(run_fn)
 end
 
-function clean_output_files()
+function clean_files()
+    delete *.avl
+    delete *.run
     delete *.fs
-    delete *.ft
+    delete *.st
 end
