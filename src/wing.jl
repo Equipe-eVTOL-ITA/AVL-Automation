5643dbaf7b69a594394d66ebaf7c92a5ae360a0f
pekpuglia <pepuglia@gmail.com>
2023-02-03 19:23:17 -0300

    control added

--- src/wing.jl
+++ src/wing.jl
@@ -1,6 +1,29 @@
 #permite futura adição de asa afilada
 abstract type AbstractWing end
 
+@enum SgnDup Equal Inverted
+struct Control
+    name::String
+    gain::Float64
+    x_c_hinge::Float64
+    #usar XYZhvec = 0 0 0 p/ usar hinge (p. 13)
+    sgn_dup::SgnDup
+end
+
+Base.Int(sd::SgnDup) = begin
+    if sd == Equal
+        1
+    elseif sd == Inverted
+        -1
+    end
+end
+
+function avl_string(c::Control)
+    "CONTROL\n" *
+    "#nome, ganho, x_c dobradiça, dobradiça 0 0 0, sinal do duplicado\n" * 
+    join([c.name, string(c.gain), string(c.x_c_hinge), "0 0 0", string(Int(c.sgn_dup))], " ")
+end
+
 #colocar controle
 struct WingSection 
     leading_edge_relative_to_wing_root::SVector{3, Unitful.Length}
@@ -10,6 +33,7 @@ struct WingSection
     claf::Float64
     cd::SVector{3, Float64}
     cl::SVector{3, Float64}
+    control::Union{Nothing, Control}
 end
 
 using Base.Iterators: flatten
@@ -29,7 +53,9 @@ function avl_string(ws::WingSection)
         string(ws.claf),
         "#CL1 CD1 CL2 CD2 CL3 CD3 para o aerofólio",
         "CDCL",
-        join(flatten(zip(ws.cl, ws.cd)), " ")
+        join(flatten(zip(ws.cl, ws.cd)), " "),
+        "#controle",
+        (isnothing(ws.control) ? "#não há" : avl_string(ws.control))
     ], "\n")*"\n"
 end
 
@@ -51,14 +77,15 @@ function avl_string(w::Wing)
         join(string.(w.vortex_distribution), " "),
         "#simetria geométrica no plano y=0",
         (w.is_symmetric) ? "YDUPLICATE" : "#não há simetria",
-        (w.is_symmetric) ? "0" : "#ângulo de incidência global",
-         "ANGLE",
-         string(ustrip(u"°", w.incidence)),
-         "#id de componente",
-         "COMPONENT",
-         string(w.component),
-         "#posição da raiz da asa",
-         "TRANSLATE",
-         join(string.(ustrip.(u"m", w.root_position)), " ")
+        (w.is_symmetric) ? "0" : "#####",
+        "#ângulo de incidência global",
+        "ANGLE",
+        string(ustrip(u"°", w.incidence)),
+        "#id de componente",
+        "COMPONENT",
+        string(w.component),
+        "#posição da raiz da asa",
+        "TRANSLATE",
+        join(string.(ustrip.(u"m", w.root_position)), " ")
     ], "\n") * "\n" * prod(avl_string.(w.sections))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,18 @@
-using avl_automation, Test
+using avl_automation, Test, Unitful
 
 @test begin
+    control = avl_automation.Control("prof", 1.0, 0.75, avl_automation.Equal)
+    avl_str = avl_automation.avl_string(control)
+    avl_str ==
+"CONTROL
+#nome, ganho, x_c dobradiça, dobradiça 0 0 0, sinal do duplicado
+prof 1.0 0.75 0 0 0 1"
+end
+
+@test begin
+    control = avl_automation.Control("prof", 1.0, 0.75, avl_automation.Equal)
     sect = avl_automation.WingSection([0u"m", 0u"m", 0u"m"], 0.3u"m", 1u"°", 
-        "naca0012_selig.dat", 1.09, [0.04703, 0.01201, 0.05415], [-1.0553, 0, 1.055])
+        "naca0012_selig.dat", 1.09, [0.04703, 0.01201, 0.05415], [-1.0553, 0, 1.055], control)
     avl_str = avl_automation.avl_string(sect)
     avl_str == 
 "#x, y, z do bordo de ataque, corda, incidência
@@ -17,12 +27,14 @@ CLAF
 #CL1 CD1 CL2 CD2 CL3 CD3 para o aerofólio
 CDCL
 -1.0553 0.04703 0.0 0.01201 1.055 0.05415
-"
+#controle
+" * avl_automation.avl_string(control) * "\n"
 end
 
 @test begin
+    control = avl_automation.Control("prof", 1.0, 0.75, avl_automation.Equal)
     sect = avl_automation.WingSection([0u"m", 0u"m", 0u"m"], 0.3u"m", 1u"°", 
-        "naca0012_selig.dat", 1.09, [0.04703, 0.01201, 0.05415], [-1.0553, 0, 1.055])
+        "naca0012_selig.dat", 1.09, [0.04703, 0.01201, 0.05415], [-1.0553, 0, 1.055], control)
     wing = avl_automation.Wing("main", [15, 1, 40, 1], true, 1.5u"°", 2, [1.0u"m", 0u"m", 0u"m"], [sect])
     avl_str = avl_automation.avl_string(wing)
     avl_str ==
@@ -33,6 +45,7 @@ main
 #simetria geométrica no plano y=0
 YDUPLICATE
 0
+#ângulo de incidência global
 ANGLE
 1.5
 #id de componente
